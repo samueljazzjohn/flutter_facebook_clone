369a1f23952ad53e2bd0930034c6e8ba1733eabd
jazzjohn <jazzjohn1999@gmail.com>
2021-11-11 20:21:00 +0530

    Header Section added

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_clone/Sections/headerSectionButtons.dart';
 import 'package:flutter_facebook_clone/Sections/statusSection.dart';
 import 'package:flutter_facebook_clone/Widgets/appBarWidget.dart';
 import '';
@@ -38,6 +39,15 @@ class Home extends StatelessWidget {
         body: ListView(
           children: [
             StatusSection(),
+            Divider(
+              thickness: 1,
+              color: Colors.grey[300],
+            ),
+            HeaderSectionButtons(),
+            Divider(
+              thickness: 10,
+              color: Colors.grey[300],
+            ),
           ],
         ),
       ),
